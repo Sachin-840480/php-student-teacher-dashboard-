0b51501f6e6341f53150a98cb2a89cc424d42a62
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-07-01 13:15:08 +0530

    fee and login php file added

---
+++ backend/api/fees.php
@@ -0,0 +1,74 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET,POST,OPTIONS");
+
+if($_SERVER['REQUEST_METHOD']=="OPTIONS"){
+exit;
+}
+
+include("../config/database.php");
+
+$method=$_SERVER['REQUEST_METHOD'];
+
+switch($method){
+
+case "GET":
+
+$sql="SELECT
+students.student_id,
+students.student_name,
+students.fee AS total_fee,
+IFNULL(SUM(fee_payments.paid_fee),0) AS paid_fee,
+(students.fee-IFNULL(SUM(fee_payments.paid_fee),0)) AS pending_fee
+FROM students
+LEFT JOIN fee_payments
+ON students.student_id=fee_payments.student_id
+GROUP BY students.student_id";
+
+$result=$conn->query($sql);
+
+$data=[];
+
+while($row=$result->fetch_assoc()){
+$data[]=$row;
+}
+
+echo json_encode($data);
+
+break;
+
+case "POST":
+
+$data=json_decode(file_get_contents("php://input"),true);
+
+$total=$data['total_fee'];
+
+$stmt=$conn->prepare("INSERT INTO fee_payments(student_id,total_fee,paid_fee,payment_date,remarks)
+VALUES(?,?,?,?,?)");
+
+$stmt->bind_param(
+"iddss",
+$data['student_id'],
+$total,
+$data['paid_fee'],
+$data['payment_date'],
+$data['remarks']
+);
+
+$stmt->execute();
+
+echo json_encode([
+"success"=>true,
+"message"=>"Payment Saved"
+]);
+
+break;
+
+}
+
+$conn->close();
+
+?>
---
+++ backend/api/login.php
@@ -0,0 +1,56 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
+include("../config/database.php");
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$username = trim($data['username']);
+$password = trim($data['password']);
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE username=?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if($result->num_rows==0){
+
+    echo json_encode([
+        "success"=>false,
+        "message"=>"Invalid Username"
+    ]);
+    exit;
+}
+
+$user = $result->fetch_assoc();
+
+if($password==$user['password']){
+
+    echo json_encode([
+        "success"=>true,
+        "message"=>"Login Successful",
+        "user"=>$user['username'],
+        "role"=>$user['role']
+    ]);
+
+}else{
+
+    echo json_encode([
+        "success"=>false,
+        "message"=>"Wrong Password"
+    ]);
+
+}
+
+$conn->close();
+
+?>

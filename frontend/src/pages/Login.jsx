127d7c895fa598b2a1d56c84971b678df123202c
Sachin-840480 <sachin1712003@gmail.com>
2026-07-01 14:05:04 +0530

    Added new css files for modules

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import styles from "./Login.module.css";
+import styles from "../modules/Login.module.css";
 
 import { login } from "../services/api";
 

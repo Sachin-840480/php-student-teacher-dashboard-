1514d578fd74e603b4e6a247b844b4b0ab3bccc4
Shubham Bhardwaj <shubhambhardwaj9575@gmail.com>
2026-07-02 19:59:22 +0530

    Footer added

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,38 +2,23 @@ import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import styles from "../modules/Layout.module.css";
+import Footer from "./Footer";
 
 function Layout({ children }) {
+  const [open, setOpen] = useState(false);
 
-    const [open,setOpen]=useState(false);
+  return (
+    <div className={styles.layout}>
+      <Sidebar open={open} setOpen={setOpen} />
 
-    return(
+      <div className={styles.main}>
+        <Navbar setOpen={setOpen} />
 
-        <div className={styles.layout}>
-
-            <Sidebar
-                open={open}
-                setOpen={setOpen}
-            />
-
-            <div className={styles.main}>
-
-                <Navbar
-                    setOpen={setOpen}
-                />
-
-                <div className={styles.content}>
-
-                    {children}
-
-                </div>
-
-            </div>
-
-        </div>
-
-    );
-
+        <div className={styles.content}>{children}</div>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Layout;

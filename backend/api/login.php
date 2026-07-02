bf52e4d3676e5b528264181355ac262ffff7489f
Sachin-840480 <sachin1712003@gmail.com>
2026-07-02 15:39:20 +0530

    Upgraded cors to enable mobile login

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,14 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-Type: application/json");
-
-if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    http_response_code(200);
-    exit();
-}
+require_once "../config/cors.php";
 include("../config/database.php");
 
 $data=json_decode(file_get_contents("php://input"),true);
